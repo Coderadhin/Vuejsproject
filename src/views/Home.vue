3855b6b7e818e6b1cbc8e957ad2840dca10f4f9a
Coderadhin <athirarameshraj@gmail.com>
2025-10-28 22:16:37 +0530

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='home'>
+    <img src='/home-bg.png' class='bg' alt='Background' />
+    <div class='overlay'>
+      <h1>Welcome to Story Voice Studio</h1>
+      <button @click='listenStory'>Let's Read</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+function listenStory() {
+  alert('Audio feature coming soon!')
+}
+</script>
+
+<style scoped>
+.home {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.bg {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: brightness(0.85);
+}
+
+.overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: #4B0082;
+}
+
+button {
+  background-color: #E6E6FA;
+  border: none;
+  padding: 0.75rem 1.5rem;
+  border-radius: 1rem;
+  font-size: 1.1rem;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='container'>
+    <img src='/logo.png' alt='Logo' class='logo' />
+    <h2>Login</h2>
+    <input type='text' placeholder='Username' v-model='username' />
+    <input type='password' placeholder='Password' v-model='password' />
+    <button @click='login'>Login</button>
+    <a href='#'>Forgot Password?</a>
+    <a href='#'>Create Account</a>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+const router = useRouter()
+const username = ref('')
+const password = ref('')
+
+function login() {
+  router.push('/home')
+}
+</script>
